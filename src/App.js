7251b4a32946a4287d2600e28f87517dddd3b5fb
Punam Kumavat <punamkumavat01@gmail.com>
2023-06-29 17:06:22 +0530

    home section added

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import { BrowserRouter } from "react-router-dom";
 import "./App.css";
 import Navbar from "./components/Navbar";
+import Home from "./components/Home";
+import About from "./components/About";
 
 function App() {
   return (
     <>
       <BrowserRouter>
         <Navbar></Navbar>
+        <Home></Home>
+        <About></About>
       </BrowserRouter>
     </>
   );
