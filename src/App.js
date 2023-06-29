980389973a2f75c0920cc3aa577b9cdf868819ef
Punam Kumavat <punamkumavat01@gmail.com>
2023-06-30 01:06:30 +0530

    favicon added

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import { BrowserRouter } from "react-router-dom";
-import "./App.css";
 import Navbar from "./components/Navbar";
 import Home from "./components/Home";
 import About from "./components/About";
