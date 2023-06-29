00481f1417863a722481a5de7667b4e17f0d3dbb
Punam Kumavat <punamkumavat01@gmail.com>
2023-06-29 17:27:01 +0530

    about page added

--- src/components/About.js
+++ src/components/About.js
@@ -1,30 +1,19 @@
 import React from "react";
-import '../styles/about.css'
-import { FaEnvelope, FaGithub, FaLinkedin } from "react-icons/fa";
+import "../styles/about.css";
 
-function About(){
-    return(
-        <>
-        <section>
-            <div  className="container">
-                <div className="social">
-                    <FaLinkedin className="social-item"/>
-                    <FaGithub className="social-item"/>
-                    <FaEnvelope className="social-item"/>
-                </div>
-                <div className="intro">
-                    <h1>Punam Kumavat 👋</h1>
-                    <p className="typed">
-                        <span><hr/></span>                        
-                    </p>
-                    <p className="desc">"As an undergraduate student from India, I am fueled by enthusiasm to venture beyond familiar boundaries and embrace new opportunities and challenges." </p>
-                </div>
-                <div className="profile">
-                    <img src="https://images.unsplash.com/photo-1575936123452-b67c3203c357?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8aW1hZ2V8ZW58MHx8MHx8fDA%3D&auto=format&fit=crop&w=500&q=60img.jpg" alt="img"/>
-                </div>
-            </div>
-        </section>
-        </>
-    );
+function About() {
+  return (
+    <>
+      <header>
+        <h2>About Me</h2>
+      </header>
+      <p className="sub-head">- My Introduction -</p>
+      <section>
+        <div className="container">
+          
+        </div>
+      </section>
+    </>
+  );
 }
 export default About;
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,46 +1,50 @@
 import React from "react";
-import '../styles/home.css';
-import Typed from 'react-typed';
-import profileImage from '../images/profile.jpg';
-import { FaEnvelope, FaGithub, FaLinkedin, FaPaperPlane } from 'react-icons/fa';
-function Home(){
-    return(
-        <>
-        <section>
-            <div  className="container">
-                <div className="social">
-                    <FaLinkedin className="social-item"/>
-                    <FaGithub className="social-item"/>
-                    <FaEnvelope className="social-item"/>
-                </div>
-                <div className="intro">
-                    <h1>Punam Kumavat 👋</h1>
-                    <p className="typed">
-                        <span><hr/></span>
-                        <Typed
-                        strings={
-                            [
-                                "Post Graduate Student",
-                                "Indian",
-                                "Front-End Web Developer"
-                            ]
-                        }
-                        typeSpeed={40}
-                        backSpeed={30}
-                        loop
-                        />
-                    </p>
-                    <p className="desc">"As an undergraduate student from India, I am fueled by enthusiasm to venture beyond familiar boundaries and embrace new opportunities and challenges." </p>
-                    <button>
-                        Say Hello   <FaPaperPlane className="social-item"/>
-                    </button>
-                </div>
-                <div className="profile">
-                    <img src={profileImage} alt="img"/>
-                </div>
-            </div>
-        </section>
-        </>
-    );
+import "../styles/home.css";
+import Typed from "react-typed";
+import profileImage from "../images/profile.jpg";
+import { FaEnvelope, FaGithub, FaLinkedin, FaPaperPlane } from "react-icons/fa";
+function Home() {
+  return (
+    <>
+      <section>
+        <div className="container">
+          <div className="social">
+            <FaLinkedin className="social-item" />
+            <FaGithub className="social-item" />
+            <FaEnvelope className="social-item" />
+          </div>
+          <div className="intro">
+            <h1>Punam Kumavat 👋</h1>
+            <p className="typed">
+              <span>
+                <hr />
+              </span>
+              <Typed
+                strings={[
+                  "Post Graduate Student",
+                  "Indian",
+                  "Front-End Web Developer",
+                ]}
+                typeSpeed={40}
+                backSpeed={30}
+                loop
+              />
+            </p>
+            <p className="desc">
+              "As an undergraduate student from India, I am fueled by enthusiasm
+              to venture beyond familiar boundaries and embrace new
+              opportunities and challenges."
+            </p>
+            <button>
+              Say Hello <FaPaperPlane className="social-item" />
+            </button>
+          </div>
+          <div className="profile">
+            <img src={profileImage} alt="img" />
+          </div>
+        </div>
+      </section>
+    </>
+  );
 }
 export default Home;
