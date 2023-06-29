0d55ca794f7edde4542a98b743d135270710a645
Punam Kumavat <punamkumavat01@gmail.com>
2023-06-29 13:58:21 +0530

    ui:navbar

---
+++ src/components/Navbar.js
@@ -0,0 +1,42 @@
+import React from "react";
+import "../styles/navbar.css";
+import { Link } from "react-router-dom";
+
+function Navbar() {
+  return (
+    <>
+      <nav className="navbar">
+        <div className="brand">Punam Kumavat</div>
+        <ul>
+          <li>
+            <Link to="/" className="list-item">
+              Home
+            </Link>
+          </li>
+          <li>
+            <Link to="/about" className="list-item">
+              About
+            </Link>
+          </li>
+          <li>
+            <Link to="/skills" className="list-item">
+              Skills
+            </Link>
+          </li>
+          <li>
+            <Link to="/project" className="list-item">
+              Project
+            </Link>
+          </li>
+          <li>
+            <Link to="/contact" className="list-item">
+              Contact
+            </Link>
+          </li>
+        </ul>
+      </nav>
+    </>
+  );
+}
+
+export default Navbar;
