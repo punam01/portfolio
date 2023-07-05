7c3cc573a9eafab433bbfa8e5325219f06649181
Punam Kumavat <punamkumavat01@gmail.com>
2023-07-05 21:57:13 +0530

    changes about & portfolio page
    download btn works

--- src/components/About.js
+++ src/components/About.js
@@ -1,9 +1,13 @@
 import React from "react";
 import "../styles/about.css";
-import profileImage from "../images/office.jpg";
+import pdf from "../PUNAM_KUMAVAT_Resume.pdf";
 import { FaLaptop, FaMedal, FaPortrait, FaSuitcase } from "react-icons/fa";
 
 function About() {
+  const handleClick = () => {
+    //const pdfUrl = './public/PUNAM_KUMAVAT_Resume.pdf';
+    window.open(pdf, '_blank');
+  };
   return (
     <>
       <header id="about">
@@ -12,9 +16,9 @@ function About() {
       <p className="sub-head">- My Introduction -</p>
       <section className="about-section">
         <div className="about-container">
-          <div className="profile1">
+          {/*<div className="profile1">
             <img src={profileImage} alt="profile1" />
-          </div>
+          </div>*/}
           <div className="about">
             <div className="about-cards">
               <div className="card-item">
@@ -46,8 +50,8 @@ function About() {
               me to quickly acquire new skills within tight timeframes.
             </div>
             <br />
-            <button>
-              Download CV <FaPortrait />
+            <button onClick={handleClick}>
+              Download CV <FaPortrait className="social-item"/>
             </button>
             <br />
           </div>
--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,6 +1,5 @@
 import React from "react";
 import "../styles/contact.css";
-import { FaEnvelope, FaGithub, FaLinkedin } from "react-icons/fa";
 
 function Contact() {
   return (
@@ -11,40 +10,7 @@ function Contact() {
       <p className="sub-head">- Don't be shy! Hit me up! -</p>
       <section className="contact-section">
         <div className="contact-container">
-          <div className="contact-item">
-            <div className="contact-icon">
-              <FaLinkedin />
-            </div>
-            <div className="contact-details">
-              <span>
-                <small>LinkedIn</small>
-              </span>
-              <span>punam-kumavat</span>
-            </div>
-          </div>
-          <div className="contact-item">
-            <div className="contact-icon">
-              <FaGithub />
-            </div>
-            <div className="contact-details">
-              <span>
-                <small>Github</small>
-              </span>
-              <span>punam01</span>
-            </div>
-          </div>
-          <br />
-          <div className="contact-item">
-            <div className="contact-icon">
-              <FaEnvelope />
-            </div>
-            <div className="contact-details">
-              <span>
-                <small>Email</small>
-              </span>
-              <span>punamkumavat01@gmail.com</span>
-            </div>
-          </div>
+          
         </div>
       </section>
     </>
