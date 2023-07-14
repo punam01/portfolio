1714499224c2c72545d72324f2822c586dd99534
Punam Kumavat <punamkumavat01@gmail.com>
2023-07-14 20:21:58 +0530

    skills section added

--- src/App.js
+++ src/App.js
@@ -6,14 +6,15 @@ import About from "./components/About";
 import Portfolio from "./components/Portfolio";
 import Contact from "./components/Contact";
 import Footer from "./components/Footer";
+import Skills from "./components/Skills";
 
 function App() {
   return (
     <>
       <Navbar></Navbar>
       <Home></Home>
+      <Skills></Skills>
       <About></About>
-      {/*<Qualification></Qualification>*/}
       <Portfolio></Portfolio>
       <Contact></Contact>
       <Footer></Footer>
--- src/components/Skills.js
+++ src/components/Skills.js
@@ -9,7 +9,40 @@ function Skills() {
       </header>
       <p className="sub-head">- An Overview of my skills -</p>
       <section>
-        <div className="container"></div>
+        <div className="skills-section">
+          <div className="skills-container">
+            <div class="logos">
+              <ul>
+                <li>
+                  <img src="https://raw.githubusercontent.com/devicons/devicon/master/icons/c/c-original.svg" alt="skill-icon"/>
+                </li>
+                <li>
+                  <img src="https://raw.githubusercontent.com/devicons/devicon/master/icons/cplusplus/cplusplus-original.svg" alt="skill-icon"/>
+                </li>
+                <li>
+                  <img src="https://raw.githubusercontent.com/devicons/devicon/master/icons/react/react-original-wordmark.svg" alt="skill-icon"/>
+                </li>
+                <li>
+                  <img src="https://raw.githubusercontent.com/devicons/devicon/master/icons/html5/html5-original-wordmark.svg" alt="skill-icon"/>
+                </li>
+                <li>
+                  <img src="https://raw.githubusercontent.com/devicons/devicon/master/icons/css3/css3-original-wordmark.svg" alt="skill-icon"/></li>
+                <li>
+                  <img src="https://raw.githubusercontent.com/devicons/devicon/master/icons/bootstrap/bootstrap-plain-wordmark.svg"  alt="skill-icon"/></li>
+                <li>
+                  <img src="https://www.vectorlogo.zone/logos/unity3d/unity3d-icon.svg"  alt="skill-icon"/></li>
+                <li>
+                  <img src="https://raw.githubusercontent.com/devicons/devicon/master/icons/python/python-original.svg" alt="skill-icon"/></li>
+                  <li>
+                    <img src="https://www.vectorlogo.zone/logos/git-scm/git-scm-icon.svg" alt="skill-icon"/>
+                  </li>
+                  <li>
+                    <img src="https://www.vectorlogo.zone/logos/figma/figma-icon.svg" alt="skill-icon"/>
+                  </li>
+                </ul>
+              </div>
+          </div>
+        </div>
       </section>
     </>
   );
