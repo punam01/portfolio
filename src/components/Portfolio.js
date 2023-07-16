dfe721c6aaef4486e5d868e6a062da07bfa94ff6
Punam Kumavat <punamkumavat01@gmail.com>
2023-07-16 16:14:23 +0530

    added figma links

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -7,6 +7,8 @@ import BirdImage from "../images/4.jpg";
 import ChintanImage from "../images/1.jpg";
 import WeatherImage from "../images/2.jpg";
 import HCCImage from "../images/3.jpg";
+import Figma1 from "../images/figma1.JPG";
+import Figma2 from "../images/figma2.JPG";
 
 function Portfolio() {
   const [activeTab, setActiveTab] = useState("all");
@@ -46,6 +48,12 @@ function Portfolio() {
             onClick={() => openTab("unity")}
           >
             Unity
+          </p>
+          <p
+            className={`tab-links ${activeTab === "figma" ? "current" : ""}`}
+            onClick={() => openTab("figma")}
+          >
+            Figma
           </p>
         </div>
         <div
@@ -123,8 +131,43 @@ function Portfolio() {
                 </div>
               </div>
             </li>
+            <li>
+              <div className="project-card">
+                <div className="project-card-item">
+                  <img src={Figma1} alt="event" />
+                </div>
+                <div className="project-card-title">
+                  Event Booking
+                  <br />
+                  <span className="project-card-link">
+                    Figma Link{" "}
+                    <a href="https://www.figma.com/proto/PnKTVWfPZszZWR4YRseKMn/Event-Booking?node-id=10-153&starting-point-node-id=10%3A153&mode=design&t=c6YgDdJIlOACqZHi-1">
+                      <PiLinkThin className="link-icon" />
+                    </a>
+                  </span>
+                </div>
+              </div>
+            </li>
+            <li>
+              <div className="project-card">
+                <div className="project-card-item">
+                  <img src={Figma2} alt="ttt" />
+                </div>
+                <div className="project-card-title">
+                  Tic-Tac-Toe Prototype
+                  <br />
+                  <span className="project-card-link">
+                    Figma Link{" "}
+                    <a href="https://www.figma.com/proto/zHeDAFIoZ3ZfSTHVnkyHdx/Tic-Tac-Toe?type=design&t=2ibXjR3EfuKaBYHc-1&scaling=scale-down&page-id=0%3A1&starting-point-node-id=25%3A141&node-id=25-141&mode=design">
+                      <PiLinkThin className="link-icon" />
+                    </a>
+                  </span>
+                </div>
+              </div>
+            </li>
           </ul>
         </div>
+        
         <div
           className={`project-container ${
             activeTab === "web" ? "active-tab" : ""
@@ -220,7 +263,51 @@ function Portfolio() {
               </div>
             </li>
           </ul>
+        </div>       
+        <div
+          className={`project-container ${
+            activeTab === "figma" ? "active-tab" : ""
+          }`}
+          id="figma"
+        >
+          <ul className="pul">
+            <li>
+              <div className="project-card">
+                <div className="project-card-item">
+                  <img src={Figma1} alt="event" />
+                </div>
+                <div className="project-card-title">
+                  Event Booking
+                  <br />
+                  <span className="project-card-link">
+                    Figma Link{" "}
+                    <a href="https://www.figma.com/proto/PnKTVWfPZszZWR4YRseKMn/Event-Booking?node-id=10-153&starting-point-node-id=10%3A153&mode=design&t=c6YgDdJIlOACqZHi-1">
+                      <PiLinkThin className="link-icon" />
+                    </a>
+                  </span>
+                </div>
+              </div>
+            </li>
+            <li>
+              <div className="project-card">
+                <div className="project-card-item">
+                  <img src={Figma2} alt="ttt" />
+                </div>
+                <div className="project-card-title">
+                  Tic-Tac-Toe Prototype
+                  <br />
+                  <span className="project-card-link">
+                    Figma Link{" "}
+                    <a href="https://www.figma.com/proto/zHeDAFIoZ3ZfSTHVnkyHdx/Tic-Tac-Toe?type=design&t=2ibXjR3EfuKaBYHc-1&scaling=scale-down&page-id=0%3A1&starting-point-node-id=25%3A141&node-id=25-141&mode=design">
+                      <PiLinkThin className="link-icon" />
+                    </a>
+                  </span>
+                </div>
+              </div>
+            </li>
+          </ul>
         </div>
+        
       </section>
     </>
   );
