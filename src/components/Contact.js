3a0002341a07444aa66c25fda903b49c92b1253c
Punam Kumavat <punamkumavat01@gmail.com>
2023-07-19 19:36:39 +0530

    contact section

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,7 +1,10 @@
 import React from "react";
 import "../styles/contact.css";
-import { FaPaperPlane } from "react-icons/fa";
-
+import {
+  PiGithubLogoLight,
+  PiLinkedinLogoLight,
+  PiChatCenteredDotsLight,
+} from "react-icons/pi";
 function Contact() {
   return (
     <>
@@ -12,23 +15,22 @@ function Contact() {
       <section className="contact-section">
         <div className="contact-container">
           <div className="contact-item">
-            <form className="contact-form"> 
-              <div className="contact-form-div">
-                <label className="contact-form-tag">Name</label>
-                <input type="text" name="name" placeholder="Insert your name" className="contact-form-input" />
-              </div>
-              <div className="contact-form-div">
-                <label className="contact-form-tag">Mail</label>
-                <input type="email" name="email" placeholder="Insert your email" className="contact-form-input" />
-              </div>
-              <div className="contact-form-div">
-                <label className="contact-form-tag">Message</label>
-                <textarea name="message" placeholder="Insert your message" cols="30" rows="10" className="contact-form-area"></textarea>
-              </div>
-              <button>
-              Sent Message <FaPaperPlane className="social-item" />
+            <PiLinkedinLogoLight className="contact-icon" />
+            <button className="contact-button" onClick={() => handleChatNow()}>
+              Chat Now
             </button>
-            </form>
+          </div>
+          <div className="contact-item">
+            <PiChatCenteredDotsLight className="contact-icon" />
+            <button className="contact-button" onClick={() => handleEmail()}>
+              Email
+            </button>
+          </div>
+          <div className="contact-item">
+            <PiGithubLogoLight className="contact-icon" />
+            <button className="contact-button" onClick={() => handleGithub()}>
+              Github
+            </button>
           </div>
         </div>
       </section>
